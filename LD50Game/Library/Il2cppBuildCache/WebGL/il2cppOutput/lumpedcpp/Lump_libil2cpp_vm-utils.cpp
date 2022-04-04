b99716ff4415de753c5372ee1f88e12cfe67ee96
polarcube1 <polarcube1@gmail.com>
2022-04-03 23:12:45 -0400

    Resetting Repo

--- LD50Game/Library/Il2cppBuildCache/WebGL/il2cppOutput/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Drive\Unity Projects\Unity\Editor\Data\il2cpp\libil2cpp\vm-utils\BlobReader.cpp"
-#include "D:\Drive\Unity Projects\Unity\Editor\Data\il2cpp\libil2cpp\vm-utils\Debugger.cpp"
-#include "D:\Drive\Unity Projects\Unity\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "D:\Drive\Unity Projects\Unity\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "D:\Drive\Unity Projects\Unity\Editor\Data\il2cpp\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "D:\Drive\Unity Projects\Unity\Editor\Data\il2cpp\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "D:\Drive\Unity Projects\Unity\Editor\Data\il2cpp\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
